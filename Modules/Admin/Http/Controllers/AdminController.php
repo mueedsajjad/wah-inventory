59f06487c9c98d8ad215a8f1317cee2f8eaa4489
Muzamil.khan <khan.muzamil007@gmail.com>
2020-04-05 20:40:21 +0500

    done

--- Modules/Admin/Http/Controllers/AdminController.php
+++ Modules/Admin/Http/Controllers/AdminController.php
@@ -387,7 +387,7 @@ class AdminController extends Controller
      */
     public function index()
     {
-        return view('admin::index');
+        return view('admin::dashboard');
     }
 
     /**
