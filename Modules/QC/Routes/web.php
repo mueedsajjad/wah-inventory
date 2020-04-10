1fc1a37b5e55a60093d07b2bc4864157dc98ed5f
Muhammad Faizan <faizanidrees19@gmail.com>
2020-04-10 12:43:07 +0500

    faizi

--- Modules/QC/Routes/web.php
+++ Modules/QC/Routes/web.php
@@ -12,18 +12,18 @@
 */
 
 Route::prefix('qc')->group(function() {
-    Route::get('/', 'QCController@index');
-    Route::get('dashboard', 'QCController@dashboard');
-    Route::get('dash', 'QCController@dash');
-    Route::get('dash/rejected', 'QCController@rejected');
-    Route::get('dash/list', 'QCController@list');
-    Route::get('inward_qc', '\Modules\Store\Http\Controllers\StoreController@inwardInspectionNote');
-    Route::get('inward_qc_out', '\Modules\Store\Http\Controllers\StoreController@inwardInspectionNote_out');
+    Route::get('/', 'QCController@index')->middleware('auth');
+    Route::get('dashboard', 'QCController@dashboard')->middleware('auth');
+    Route::get('dash', 'QCController@dash')->middleware('auth');
+    Route::get('dash/rejected', 'QCController@rejected')->middleware('auth');
+    Route::get('dash/list', 'QCController@list')->middleware('auth');
+    Route::get('inward_qc', '\Modules\Store\Http\Controllers\StoreController@inwardInspectionNote')->middleware('auth');
+    Route::get('inward_qc_out', '\Modules\Store\Http\Controllers\StoreController@inwardInspectionNote_out')->middleware('auth');
 
 
 
-    Route::get('production-product', 'QCController@productionProduct');
-    Route::get('production-component', 'QCController@productionComponent');
+    Route::get('production-product', 'QCController@productionProduct')->middleware('auth');
+    Route::get('production-component', 'QCController@productionComponent')->middleware('auth');
 
     Route::post('component-inspection', 'QCController@componentInspection');
     Route::post('product-inspection', 'QCController@productInspection');
