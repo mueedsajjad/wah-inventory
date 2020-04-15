166532cef456fb97344b66de1f5130c9ce2dd791
kaleem <kaleemshoukat96@gmail.com>
2020-04-15 20:44:30 +0500

    worked on sale and gate

--- Modules/Gate/Http/Controllers/GateController.php
+++ Modules/Gate/Http/Controllers/GateController.php
@@ -281,6 +281,8 @@ class GateController extends Controller
         ];
         $insert=DB::table('vehicle_management')->insert($data);
 
+
+
         if ($insert){
             return redirect()->back()->with('message', 'Submitted Successfuly.');
         }
@@ -412,65 +414,10 @@ class GateController extends Controller
         return view('gate::index');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     * @return Response
-     */
-    public function create()
-    {
-        return view('gate::create');
-    }
 
-    /**
-     * Store a newly created resource in storage.
-     * @param Request $request
-     * @return Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
 
-    /**
-     * Show the specified resource.
-     * @param int $id
-     * @return Response
-     */
-    public function show($id)
-    {
-        return view('gate::show');
-    }
 
-    /**
-     * Show the form for editing the specified resource.
-     * @param int $id
-     * @return Response
-     */
-    public function edit($id)
-    {
-        return view('gate::edit');
-    }
-
-    /**
-     * Update the specified resource in storage.
-     * @param Request $request
-     * @param int $id
-     * @return Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     * @param int $id
-     * @return Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+   
     public function vendor_data($id){
 //      dd($id);
         $vendors=DB::table('purchase_order_approval')->find($id);
